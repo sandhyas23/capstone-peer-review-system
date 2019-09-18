1a644220f3c987caa4ab9f4b01a28d5da1db9199
Sandhya <ssankaran3@horizon.csueastbay.edu>
2019-09-18 11:02:32 -0700

    Worked on the login component in app.js. Removed unwanted filed under semantic and updated the package.json for client accordingly.

--- packages/client-peer-review-system/src/app.js
+++ packages/client-peer-review-system/src/app.js
@@ -1,57 +1,110 @@
 import React from 'react';
-import { Dropdown,Icon } from 'semantic-ui-react'
-import tasks from './tasks1.json';
-import TaskViewer from "./taskViewer";
+import {Button, Form, Grid,Header,Segment,Message} from 'semantic-ui-react'
+import students from './students.json';
+import StudentView from './studentView';
+import TeacherView from './TeacherView';
+
 
 
 export default class App extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { tasks: tasks, currentTask:"", mode:"none" };
-    }
-    // componentDidMount(){
-    //     let _this = this;
-    //     _this.setState({sampleTasks: tasks});
-    // }
-
-    showTask(eventKey,data) {
-        this.setState({mode:"view", currentTask: data.text});
-        console.log("dksdsd" +data.text);
-        console.log(data.text);
-
+        this.state = { netId: "" , password:"" , students:students, role:"", message:""};
     }
 
-    showQuestions(){
-        const isTask = this.state.mode;
-        if (isTask === "view") {
-            return <TaskViewer tasks={this.state.tasks}  currentTask ={this.state.currentTask} mode={this.state.mode} />
-        }
-        else{
-            return <div>
-
-            </div>
-        }
-
-    }
-
-    render() {
-        let listItems =  this.state.tasks.map((task, index, array) => {
-            return <Dropdown.Item text = {task["task-name"]} key = {`task${index}`} onClick = {(eventKey,data) => this.showTask(eventKey,data)}
-                                   value={task["task-name"]}/>
-
+    handleChange(event) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+        this.setState({
+            [name]: value
         });
 
-        return  <div>
-            <Icon name='tag' />
-            <Dropdown text={"TaskID"} >
-                <Dropdown.Menu>
-            {listItems}
-            </Dropdown.Menu>
-            </Dropdown>
-            {this.showQuestions()}
-        </div>
+    }
+    login() {
+        //console.log(this.state.netId, this.state.password);
+        const userId = this.state.netId;
+        const userPwd = this.state.password;
+
+        const user = this.state.students.find((student, index, array) => {
+            return (student.netid === this.state.netId && student.password === this.state.password)
+        });
+        //console.log(user.role);
+        if(typeof user ==="undefined"){
+            this.setState({netId: "" , password: "" ,message:"Invalid credentials"})
+        }
+        else{
+            if(user.role === "instructor"){
+                this.setState({role:"instructor"});
+            }
+            else if(user.role === "student"){
+                this.setState({role:"student"});
+            }
+            else{
+                this.setState({ role:"none"});
+            }
+        }
 
 
+
+    }
+
+
+    render() {
+        if(this.state.role === "") {
+            return<Grid centered columns={2}>
+                <Grid.Column>
+                    <Header as="h2" textAlign="center">
+                        Login
+                    </Header>
+                    <Segment>
+                        <Form size="large">
+                            <Form.Input
+                                fluid
+                                icon="user"
+                                iconPosition="left"
+                                placeholder="Net ID"
+                                name="netId"
+                                value={this.state.netId}
+                                onChange={(event) => this.handleChange(event)}
+                            />
+                            <Form.Input
+                                fluid
+                                icon="lock"
+                                iconPosition="left"
+                                placeholder="Password"
+                                type="password"
+                                name="password"
+                                value={this.state.password}
+                                onChange={(event) => this.handleChange(event)}
+                            />
+                            <Button onClick={() => this.login()} color="blue" fluid size="large">
+                                Login
+                            </Button>
+                        </Form>
+                    </Segment>
+                    <Message>
+                        <Header as='h4' color='red'>
+                            {this.state.message}
+                        </Header>
+                        Having trouble ? Contact the Instructor
+                    </Message>
+                </Grid.Column>
+            </Grid>
+
+        }
+
+        else if(this.state.role === "instructor"){
+            return <TeacherView />
+        }
+
+        else if(this.state.role === "student"){
+            return <StudentView />
+        }
+
+        else if(this.state.role === "none"){
+            return <div>not authorized</div>
+        }
     }
 }
 
