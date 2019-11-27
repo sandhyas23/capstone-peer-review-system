f1954543b734cc8461ac60f5c1c7f87deeac1b43
Sandhya <ssankaran3@horizon.csueastbay.edu>
2019-11-27 10:57:22 -0800

    Updated CORS to allows read access in server

--- packages/server-peer-review-system/routes/login.js
+++ packages/server-peer-review-system/routes/login.js
@@ -23,8 +23,14 @@ router.use(function(req, res, next) {
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-    next();
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    // allow preflight
+    if (req.method === 'OPTIONS') {
+        res.send(200);
+    } else {
+        next();
+    }
+    //next();
 });
 
 // Validate login for teacher and students
--- packages/server-peer-review-system/routes/submissiontaskRoute.js
+++ packages/server-peer-review-system/routes/submissiontaskRoute.js
@@ -11,6 +11,12 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    // allow preflight
+    if (req.method === 'OPTIONS') {
+        res.send(200);
+    } else {
+        next();
+    }
     next();
 });
 const submissionTaskDb = require("../models/submissionTaskModel");
